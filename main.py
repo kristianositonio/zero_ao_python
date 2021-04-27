4347458847f91a4852a10025804c0ac43860a4d9
kristianositonio <kristianositonio@gmail.com>
2021-04-26 21:47:40 -0300

    aula_1

---
+++ main.py
@@ -0,0 +1,69 @@
+# x = 5
+# y = 9
+
+# print(x)
+# print(y)
+ 
+# print('hello world')
+ 
+# z = x + y
+# print(z)
+ 
+# cor_verde = 'verde'
+ 
+# ola = 'youtube '
+# boa_noite = 'boa noite'
+# print(ola + boa_noite)
+
+# A = input('Digite um valor para A: ')
+# B = input('Digite um valor para B: ')
+
+# if A > B: 
+#   print('Champs: ', A)
+# elif B > A:
+#   print('Champs: ', B)
+# else:
+#   print('Variáveis de mesmo valor')
+  
+# mes = input('Digite o mes do seu bday: ')
+# dia = int(input('Digite o dia do seu bday: '))
+
+# if mes == 'jan':
+#   tipo = 'Show'
+# elif mes == 'fev':
+#   tipo = 'WOW'
+
+# if dia == 20:
+#   tipo += ' man'
+# elif dia == 22:
+#   tipo += ' bro'
+
+# print(tipo)
+
+#  Existe uma ordem de prioridade para o resultado quando utilizamos múltiplos operadores 
+#  lógicos e a ordem que eles são resolvidos é essa: NOT, AND, OR. 
+#  Caso queira alterar a ordem basta adicionar parenteses para as operações que devem ser feitas juntas
+
+# a = 4
+# b = 3
+
+# if a > 3 and b < 3:
+#   print('operador e')
+# elif a == 4 or b > 5:
+#   print('operador ou')
+# else:
+#   print('escape')
+
+# c = True
+
+# if c:
+#   print('false')
+# else:
+#   print('true')
+
+
+# not True
+
+a = (1 % 1)
+
+print(a)
